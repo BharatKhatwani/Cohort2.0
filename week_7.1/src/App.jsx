a14e6cf27b65680db3ff02749c48b5e8f231ee10
BharatKhatwani <bharatkhatwani06@gmail.com>
2024-06-12 17:37:06 +0530

    Context.api learn

--- week_7.1/src/App.jsx
+++ week_7.1/src/App.jsx
@@ -1,38 +1,51 @@
-import { useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 
-function App(){
-  const [count ,setCount] = useState(0);
+// Create the CountContext
+const CountContext = createContext();
+
+function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-<Count count = {count}/>
-<Buttons count = {count} setCount = {setCount}/>
+      {/* Provide the count and setCount to the entire tree */}
+      <CountContext.Provider value={{ count, setCount }}>
+        <Count />
+      </CountContext.Provider>
     </div>
-  )
+  );
 }
 
-
-
-function Count({count}){
-return (
-  <div>
-    {count}
-  </div>
-)
+function Count() {
+  return (
+    <div>
+      <CountRender /> {/* No need to pass count and setCount as props */}
+      <Buttons /> {/* No need to pass count and setCount as props */}
+    </div>
+  );
 }
-function Buttons({count,setCount}){
-return(
-  <div>
-    <button onClick={()=>{
-setCount(count+1);
-    }}>Increase</button>
-    <button onClick={()=>{
-setCount(count-1);
-    }
 
-    }>Decrease</button>
-  </div>
-)
+function CountRender() {
+  const { count } = useContext(CountContext); // Destructure count from context
+
+  return (
+    <div>
+      count = {count}
+    </div>
+  );
 }
+
+function Buttons() {
+  const { count, setCount } = useContext(CountContext); // Destructure count and setCount from context
+
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+    </div>
+  );
+}
+
 export default App;
 
 
@@ -42,9 +55,9 @@ export default App;
 
 
 
+// 3. 
 
-
-
-// 1 
-// The code of React route learn in it 
+// 2.     // code of prop drilling :-  https://gist.github.com/BharatKhatwani/38b04d6e81be63a9dfe06459d95c5849
+ 
+//  1. The code of React route learn in it 
 // Code :- https://gist.github.com/BharatKhatwani/99b5412de727f5c1b711ae92bd1da7cd
---
+++ week_7.1/src/Components/context.jsx
@@ -0,0 +1,3 @@
+import { createContext } from "react";
+
+export const CountCountext = createContext(0); // transport for some place to other
