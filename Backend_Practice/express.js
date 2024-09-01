81e2afa1909e72b909f7bd35fc4318069765b062
BharatKhatwani <bharatkhatwani06@gmail.com>
2024-09-02 00:36:59 +0530

    "backend_pratice"

---
+++ Backend_Practice/express.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+// Setting the view engine to EJS
+app.set("view engine", "ejs");
+
+// Route to render the index.ejs file
+app.get("/", function (req, res) {
+    res.render("index",{age : 45});
+});
+
+app.get("/contact" , function(req,res){
+    res.render("answer");
+})
+
+// Start the server
+app.listen(3005, () => {
+    console.log("Server is running on port 3005");
+});
---
+++ Backend_Practice/script2.js
@@ -0,0 +1,3 @@
+var c = require('./script')
+
+console.log(c);
