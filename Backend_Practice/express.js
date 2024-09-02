e6ac5e178fefeda7183ba389a752f814bace0d2a
BharatKhatwani <bharatkhatwani06@gmail.com>
2024-09-02 23:57:25 +0530

    "backend-using"

--- Backend_Practice/express.js
+++ Backend_Practice/express.js
@@ -1,19 +1,32 @@
-const express = require('express');
+// const express = require('express');
+// const app = express();
+
+// // Setting the view engine to EJS
+// app.set("view engine", "ejs");
+
+// // Route to render the index.ejs file
+// app.get("/", function (req, res) {
+//     res.render("index",{age : 45});
+// });
+
+// app.get("/contact" , function(req,res){
+//     res.render("answer");
+// })
+
+// // Start the server
+// app.listen(3005, () => {
+//     console.log("Server is running on port 3005");
+// });
+
+
+const express  = require("express");
 const app = express();
-
-// Setting the view engine to EJS
 app.set("view engine", "ejs");
+app.use(express.static("./public"))
 
-// Route to render the index.ejs file
-app.get("/", function (req, res) {
-    res.render("index",{age : 45});
-});
-
-app.get("/contact" , function(req,res){
-    res.render("answer");
+app.get("/" , function(req,res){
+    res.render("index");
 })
-
-// Start the server
 app.listen(3005, () => {
     console.log("Server is running on port 3005");
 });
---
+++ hey.js
@@ -0,0 +1,9 @@
+
+var globalVar = "I am a global variable";
+function showGlobalVar() {
+console.log(globalVar); 
+}
+showGlobalVar(); // 
+console.log(globalVar); // 
+//output:
+//I am a global variabl
